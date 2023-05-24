f06652efeb393c29af071c823a9c405d3f7d34a2
Yang Liu <ylyangliu1@gmail.com>
2023-05-24 15:02:51 -0700

    finish js logic

--- Assignments/Assignment 1/calculate.js
+++ Assignments/Assignment 1/calculate.js
@@ -4,6 +4,15 @@ function submitNewGrade() {
   let maxBound = document.getElementById("maxBound").value.trim();
   let aplusBound = document.getElementById("aplusBound").value.trim();
   let aBound = document.getElementById("aBound").value.trim();
+  let aminusBound = document.getElementById("aminusBound").value.trim();
+  let bplusBound = document.getElementById("bplusBound").value.trim();
+  let bBound = document.getElementById("bBound").value.trim();
+  let bminusBound = document.getElementById("bminusBound").value.trim();
+  let cplusBound = document.getElementById("cplusBound").value.trim();
+  let cBound = document.getElementById("cBound").value.trim();
+  let cminusBound = document.getElementById("cminusBound").value.trim();
+  let dBound = document.getElementById("dBound").value.trim();
+  let fBound = document.getElementById("fBound").value.trim();
 
   // Check for invalid inputs
   if (userInput === "") {
@@ -21,6 +30,24 @@ function submitNewGrade() {
     updateHistogram("outputValueA+");
   } else if (userInput <= aplusBound && userInput >= aBound) {
     updateHistogram("outputValueA");
+  } else if (userInput <= aBound && userInput >= aminusBound) {
+    updateHistogram("outputValueA-");
+  } else if (userInput <= aminusBound && userInput >= bplusBound) {
+    updateHistogram("outputValueB+");
+  } else if (userInput <= bplusBound && userInput >= bBound) {
+    updateHistogram("outputValueB");
+  } else if (userInput <= bBound && userInput >= bminusBound) {
+    updateHistogram("outputValueB-");
+  } else if (userInput <= bminusBound && userInput >= cplusBound) {
+    updateHistogram("outputValueC+");
+  } else if (userInput <= cplusBound && userInput >= cBound) {
+    updateHistogram("outputValueC");
+  } else if (userInput <= cBound && userInput >= cminusBound) {
+    updateHistogram("outputValueC-");
+  } else if (userInput <= cminusBound && userInput >= dBound) {
+    updateHistogram("outputValueD");
+  } else if (userInput <= dBound && userInput >= fBound) {
+    updateHistogram("outputValueF");
   }
 }
 
