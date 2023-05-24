7682d59f49c8aadb0be65cfcfa04e6c78b4b0e28
yangliudev <ylyangliu1@gmail.com>
2023-05-24 00:24:37 -0700

    working on logic after submit button is clicked

--- Assignments/Assignment 1/calculate.js
+++ Assignments/Assignment 1/calculate.js
@@ -1,5 +1,13 @@
-function submitForm() {
-  var newGradeInput = document.getElementById("newGradeInput");
-  var newGrade = newGradeInput.value;
-  alert("New grade submitted: " + newGrade);
+function submitNewGrade() {
+  let userInput = document.getElementById("newGradeInput").value.trim();
+
+  if (userInput === "") {
+    alert("Please enter a value.");
+  }
+
+  if (isNaN(userInput) || userInput < 0) {
+    alert("Please enter a valid number greater than 0.");
+  }
+
+  document.getElementById("outputValueA+").textContent = userInput;
 }
