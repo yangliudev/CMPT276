8e4bfe4f54a7a5b53f503f83a8af3f484d1480f1
Yang Liu <ylyangliu1@gmail.com>
2023-05-24 14:51:15 -0700

    work on js logic

--- Assignments/Assignment 1/calculate.js
+++ Assignments/Assignment 1/calculate.js
@@ -1,6 +1,11 @@
 function submitNewGrade() {
+  // Get necessary html elements using ID
   let userInput = document.getElementById("newGradeInput").value.trim();
+  let maxBound = document.getElementById("maxBound").value.trim();
+  let aplusBound = document.getElementById("aplusBound").value.trim();
+  let aBound = document.getElementById("aBound").value.trim();
 
+  // Check for invalid inputs
   if (userInput === "") {
     alert("Please enter a value.");
   }
@@ -9,5 +14,29 @@ function submitNewGrade() {
     alert("Please enter a valid number greater than 0.");
   }
 
-  document.getElementById("outputValueA+").textContent = userInput;
+  userInput = parseInt(userInput);
+
+  // Calculate where to place the new grade on histogram
+  if (userInput <= maxBound && userInput >= aplusBound) {
+    updateHistogram("outputValueA+");
+  } else if (userInput <= aplusBound && userInput >= aBound) {
+    updateHistogram("outputValueA");
+  }
+}
+
+function updateBound(bound) {
+  if (bound === "maxBound") {
+    console.log("max bound");
+  } else if (bound === "aplusBound") {
+    console.log("a plus bound");
+  } else {
+    console.log("hello world");
+  }
+}
+
+function updateHistogram(elementToUpdate) {
+  let outputElement = document.getElementById(elementToUpdate);
+  let existingText = outputElement.textContent;
+  let newText = existingText + "O";
+  outputElement.textContent = newText;
 }
