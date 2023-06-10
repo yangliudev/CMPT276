b80cfb30d647bc1a151994e4242d04388be87fde
Yang Liu <ylyangliu1@gmail.com>
2023-06-10 11:27:37 -0700

    add asn2

---
+++ Assignments/Assignment 2/asn2/src/main/java/com/asn2/asn2/Asn2Application.java
@@ -0,0 +1,13 @@
+package com.asn2.asn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asn2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Asn2Application.class, args);
+	}
+
+}
---
+++ Assignments/Assignment 2/asn2/src/test/java/com/asn2/asn2/Asn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asn2.asn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asn2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
